19e319296676548953ae0c03610600e1dd34851e
Larissa <larissagomes8020@gmail.com>
2024-05-02 20:05:53 -0300

    personagem se movendo no eixo X

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ y = 720
 screen = pygame.display.set_mode((x,y))
 pygame.display.set_caption('Recurssao aninhada game')
 
-bg = pygame.image.load('./imagens/fundo_galaxia.jpg').convert_alpha()
+bg = pygame.image.load('./imagens/fundo_laranja.png').convert_alpha()
 bg = pygame.transform.scale(bg, (x,y))
 
 inimigo = pygame.image.load('./imagens/inimigo1t.png').convert_alpha()
 inimigo = pygame.transform.scale(inimigo, (50,50))
 
-personagem = pygame.image.load('./imagens/picles1t.png').convert_alpha()
+personagem = pygame.image.load('./imagens/gato_verde.png').convert_alpha()
 personagem = pygame.transform.scale(bg, (80,80))
 personagem = pygame.transform.rotate(personagem, -90)
 
@@ -42,8 +42,13 @@ while rodando:
         pos_personagem_y -=1
     if tecla[pygame.K_DOWN] and pos_personagem_y < 665 :
         pos_personagem_y +=1
+    if tecla[pygame.K_RIGHT] and pos_personagem_x > 1 :
+        pos_personagem_x +=1
+    if tecla[pygame.K_LEFT] and pos_personagem_x < 1275 :
+        pos_personagem_x -=1
 
     x-=1 #movimento do fundo
+    pos_inimigo_x -=1
 
 
     screen.blit(inimigo,(pos_inimigo_x, pos_inimigo_y))
