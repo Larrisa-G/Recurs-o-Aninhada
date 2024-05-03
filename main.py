7bb96784f477cd945c1a4c0c12ffaefb46990e51
Larissa <larissagomes8020@gmail.com>
2024-05-02 21:41:03 -0300

    criada a luz acompanhando o personagem

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ pygame.display.set_caption('Recurssao aninhada game')
 bg = pygame.image.load('./imagens/fundo_laranja.png').convert_alpha()
 bg = pygame.transform.scale(bg, (x,y))
 
-inimigo = pygame.image.load('./imagens/inimigo1t.png').convert_alpha()
+inimigo = pygame.image.load('./imagens/cat_idle.png').convert_alpha()
 inimigo = pygame.transform.scale(inimigo, (50,50))
 
-personagem = pygame.image.load('./imagens/gato_verde.png').convert_alpha()
-personagem = pygame.transform.scale(bg, (80,80))
-personagem = pygame.transform.rotate(personagem, -90)
+personagem = pygame.image.load('./imagens/cat_idle_small.png').convert_alpha()
+personagem = pygame.transform.scale(inimigo, (300,300))
+personagem = pygame.transform.rotate(personagem, 0)
+
+luz = pygame.image.load('./imagens/luzt.png').convert_alpha()
+luz = pygame.transform.scale(luz,(25,25))
+luz = pygame.transform.rotate(luz, -45)
 
 pos_inimigo_x = 500
 pos_inimigo_y = 360
@@ -24,7 +28,11 @@ pos_inimigo_y = 360
 pos_personagem_x = 200
 pos_personagem_y = 300
 
+pos_luz_x = 400
+pos_luz_y = 300
+velocidade_luz = 0
 
+triggered = False
 rodando = True
 
 def respawn():
@@ -48,26 +56,48 @@ while rodando:
     tecla = pygame.key.get_pressed()
     if tecla[pygame.K_UP] and pos_personagem_y > 1 :
         pos_personagem_y -=velocidade_personagem 
+        pos_luz_y -=velocidade_personagem 
+        if not triggered :
+            pos_luz_x -= velocidade_personagem
     if tecla[pygame.K_DOWN] and pos_personagem_y < 665 :
         pos_personagem_y +=velocidade_personagem 
+        pos_luz_y +=velocidade_personagem 
+        if not triggered :
+            pos_luz_x += velocidade_personagem
     if tecla[pygame.K_RIGHT] and pos_personagem_x < 1200 :
         pos_personagem_x +=velocidade_personagem 
+        pos_luz_x +=velocidade_personagem 
+        if not triggered :
+            pos_luz_x += velocidade_personagem
     if tecla[pygame.K_LEFT] and pos_personagem_x  > 1 :
         pos_personagem_x -= (velocidade_personagem + 0.5)
+        pos_luz_x -= (velocidade_personagem + 0.5)
+        if not triggered :
+            pos_luz_x -= velocidade_personagem
 
-    #
+
+    # inimigo
     if pos_inimigo_x == 50:
         pos_inimigo_x = respawn()[0]
         pos_inimigo_y = respawn()[1]
 
+    #luz
+    if tecla[pygame.K_SPACE] :
+        triggered = True
+        velocidade_luz = 1
+    
+   
+
     #velocidade dos movimentos
     x-=1 
     velocidade_personagem = 2
-    pos_inimigo_x -=5
+    pos_inimigo_x -=2
+    pos_luz_x +=velocidade_luz
 
     #mostra na teal
     screen.blit(inimigo,(pos_inimigo_x, pos_inimigo_y))
     screen.blit(personagem, (pos_personagem_x, pos_personagem_y))
+    screen.blit(luz, (pos_personagem_x, pos_personagem_y))
 
 
     pygame.display.update()
