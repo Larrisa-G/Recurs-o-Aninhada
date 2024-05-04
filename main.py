57a1b26ad27e524c5ed6671ec29098dec6ecb6cc
Larissa <larissagomes8020@gmail.com>
2024-05-03 23:22:32 -0300

    Criada a classe Projetil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 import random
 
 pygame.init()
@@ -18,22 +19,29 @@ personagem = pygame.image.load('./imagens/cat_idle_small.png').convert_alpha()
 personagem = pygame.transform.scale(personagem, (300,300))
 personagem = pygame.transform.rotate(personagem, 0)
 
-luz = pygame.image.load('./imagens/luzt.png').convert_alpha()
-luz = pygame.transform.scale(luz,(25,25))
-luz = pygame.transform.rotate(luz, -45)
-
 pos_inimigo_x = 500
 pos_inimigo_y = 360
 
 pos_personagem_x = 200
 pos_personagem_y = 300
 
-pos_luz_x = 200
-pos_luz_y = 300
-velocidade_luz = 0
-
 triggered = False
 rodando = True
+
+
+class Projetil(pygame.sprite.Sprite) :
+    def __init__(self, posx, posy) :
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('./imagens/luzt.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.velocidade = 5
+        self.rect.center = (posx,posy)
+        self.velocidadade = 5
+
+    def update(self) :
+        self.rect.x = self.rect.x - self.velocidadade
+    
+all_sprites = pygame.sprite.Group()
 
 def respawn():
     x = 1350
@@ -45,32 +53,24 @@ while rodando:
         if event.type == pygame.QUIT:
             rodando = False
 
-    screen.blit(bg, (0,0)) #bg de background
+    screen.blit(bg, (0,0)) 
 
+    #cria background
     rel_x = x % bg.get_rect().width
-    screen.blit(bg,(rel_x - bg.get_rect().width,0)) #cria background
+    screen.blit(bg,(rel_x - bg.get_rect().width,0)) 
     if rel_x < 1280:
         screen.blit(bg, (rel_x,0))
     
     #movimento personagem
     tecla = pygame.key.get_pressed()
     if tecla[pygame.K_UP] and pos_personagem_y > 1 :
-        pos_personagem_y -=velocidade_personagem
-        if not triggered :
-            pos_luz_x -= velocidade_personagem
-    if tecla[pygame.K_DOWN] and pos_personagem_y < 665 :
+        pos_personagem_y -= velocidade_personagem
+    if tecla[pygame.K_DOWN] and pos_personagem_y < 505 :
         pos_personagem_y +=velocidade_personagem 
-        if not triggered :
-            pos_luz_x += velocidade_personagem
-    if tecla[pygame.K_RIGHT] and pos_personagem_x < 1200 :
-        pos_personagem_x +=velocidade_personagem 
-        if not triggered :
-            pos_luz_x += velocidade_personagem
+    if tecla[pygame.K_RIGHT] and pos_personagem_x < 1000 :
+        pos_personagem_x +=velocidade_personagem
     if tecla[pygame.K_LEFT] and pos_personagem_x  > 1 :
         pos_personagem_x -= (velocidade_personagem + 0.5)
-        if not triggered :
-            pos_luz_x -= velocidade_personagem
-
 
     # inimigo
     if pos_inimigo_x == 50:
@@ -80,18 +80,21 @@ while rodando:
     #luz
     if tecla[pygame.K_SPACE] :
         triggered = True
-        velocidade_luz = +1
+        proj_luz = Projetil(pos_personagem_x, pos_personagem_y)
+        all_sprites.add(proj_luz)
 
     #velocidade dos movimentos
     x-=1 
     velocidade_personagem = 2
     pos_inimigo_x -=5
-    pos_luz_x +=velocidade_luz
+
+    #Atualização e desenho do projétil de luz
+    if triggered:
+        all_sprites.update()
+        all_sprites.draw(screen)
 
     #mostra na teal
     screen.blit(inimigo,(pos_inimigo_x, pos_inimigo_y))
     screen.blit(personagem, (pos_personagem_x, pos_personagem_y))
-    screen.blit(luz, (pos_personagem_x, pos_personagem_y))
-
-
+        
     pygame.display.update()
